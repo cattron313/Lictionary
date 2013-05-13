11aa1aee97551344417482cf411287d8b0d804a9
Alexandria Cattron <acattron@gmail.com>
2013-05-12 18:42:05 -0700

    New rails project
    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lictionary::Application.config.secret_token = 'e0870f135be1514800a48ee952e3eb540458e7be4a11c2f1788c47aa75875def81583bda92edf00dca15d86a5007efbfbc1b9040f32ded601bffedb9682158c3'
